0abfb60da128ae2f7a4eb4af8933ca7ead050678
Leo <leo12160201@gmail.com>
2022-10-12 15:06:04 +0800

    Layout調整

--- SignalRChat/Hubs/ChatHub.cs
+++ SignalRChat/Hubs/ChatHub.cs
@@ -12,8 +12,6 @@ namespace SignalRChat.Hubs
         private readonly static ConnectionMapping<string> UserList =
              new ConnectionMapping<string>();
 
-        public static List<string> UsersList = new List<string>();
-
         public override Task OnConnectedAsync()
         {
             return base.OnConnectedAsync();
@@ -23,21 +21,20 @@ namespace SignalRChat.Hubs
         {
             string message = " 歡迎使用者 " + name + " 加入聊天室 ";
             UserList.Add(name,Context.ConnectionId);
-            UsersList.Add(name);
-            List<string> Users = UpdateUsersList();
+            List<string> UsersList = UpdatesersList();
             Clients.All.SendAsync("GetConnectedUsers", UsersList);
 
             SendMessage(message);
         }
 
-        public List<string> UpdateUsersList() {
+        public List<string> UpdatesersList() {
             return UserList.GetUsers();
         }
 
         public async Task SendMessage(string message)
         {
             var Users = UserList.GetConnection("Leo");
-            await Clients.All.SendAsync("ReceiveMessage","Leo", message);
+            await Clients.Others.SendAsync("ReceiveMessage","Leo", message);
         }
         public override Task OnDisconnectedAsync(Exception? exception)
         {
--- SignalRChat/wwwroot/js/chat.js
+++ SignalRChat/wwwroot/js/chat.js
@@ -1,43 +1,63 @@
 ﻿
-var UserName;
+var _UserName;
 var person = prompt("请输入你的名字", "Harry Potter");
 if (person != null && person != "") {
-    UserName = person;
+    _UserName = person;
 }
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub/123").build();
 
-//Disable the send button until connection is established.
-document.getElementById("sendButton").disabled = true;
-
 connection.on("ReceiveMessage", function (user, message) {
-    var li = document.createElement("li");
-    document.getElementById("messagesList").appendChild(li);
-    li.textContent = `${user} says ${message}`;
+    printMessage(false, message);
 });
 
 connection.start().then(function () {
     document.getElementById("sendButton").disabled = false;
-    connection.invoke("UserConnected", UserName);
+    connection.invoke("UserConnected", _UserName);
 
 }).catch(function (err) {
     return console.error(err.toString());
 });
 
 connection.on("GetConnectedUsers", function (UserList) {
-    document.getElementById("UserList").innerHTML = ""
-    UserList.forEach(user => {
-        var li = document.createElement("li");
-        document.getElementById("UserList").appendChild(li);
-        li.textContent = user;
-    })
+    if (UserList.includes(_UserName)) {
+        UserList.splice(UserList.indexOf(_UserName), 1);
+    } 
+    document.getElementById("usersList").innerHTML = "";
+    printConnectedUser(UserList);
 })
 
 document.getElementById("sendButton").addEventListener("click", function (event) {
-    var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
+    printMessage(true, message);
     connection.invoke("SendMessage",message).catch(function (err) {
         return console.error(err.toString());
     });
     event.preventDefault();
 });
+
+function printMessage(isOut,message) {
+
+    var eTemplate = document.querySelector(isOut ? '#chatOutcomingTemplate' : '#chatIncomingTemplate').cloneNode(true);
+    var eContent = eTemplate.querySelector('#content');
+    //eContent.innerHTML = "";
+    eContent.innerHTML = message;
+    var eTime = eTemplate.querySelector('#time');
+    //eTime.innerHTML = "";
+    eTime.innerHTML = "2022/10/11";
+    var messageMain = eTemplate.querySelector('#message');
+    document.getElementById("messagesList").appendChild(messageMain);
+
+}
+
+function printConnectedUser(UserList) {
+    UserList.forEach(user => {
+        var eTemplate = document.querySelector('#userTemplate').cloneNode(true);
+        var eUserName = eTemplate.querySelector('#userName');
+        eUserName.innerHTML = user;
+        var eTime = eTemplate.querySelector('#time');
+        eTime.innerHTML = '2022/10/11';
+        var eUserMain = eTemplate.querySelector('#userMain');
+        document.getElementById("usersList").appendChild(eUserMain);
+    })
+}
